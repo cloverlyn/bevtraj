562aeb778cf13d53bfe77c30722d16ffc2dbdd6c
MINSANG KONG <meer0725@gmail.com>
2026-03-21 10:38:50 +0900

    exp: model update

--- unitraj/models/bevtraj/loss_utils.py
+++ unitraj/models/bevtraj/loss_utils.py
@@ -28,8 +28,8 @@ class Criterion(nn.Module):
         gt_decoder = gt[0]
         gt_dense_future_trajs = gt[1]
         
-        # goal_candidate = anchor_pos
-        goal_candidate = out['goal_reg_list'][-1].permute(1, 0, 2)
+        goal_candidate = anchor_pos
+        # goal_candidate = out['goal_reg_list'][-1].permute(1, 0, 2)
         decoder_loss = self.get_decoder_loss_hard_assign(
             modes_preds=modes_preds,
             preds=preds,
